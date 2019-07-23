2e93afac48bcd520149e704c98ddf7b526c4f942
David Daza <03dazal@gmail.com>
2019-07-22 19:08:31 -0500

    stand-in static forms

--- src/views/Request.vue
+++ src/views/Request.vue
@@ -1,31 +1,31 @@
 <template>
   <div class="w-full max-w-lg mx-auto absolute top-1/2 left-1/2" style="transform: translate(-50%, -50%)">
-    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" method="post" @submit.prevent="handleSubmit">
+    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" method="post" name="request">
       <h1 class="text-center mb-8 text-xl font-bold">Háblanos de tu medio</h1>
       <input type="hidden" name="form-name" value="request" />
       <div class="mb-4">
         <label class="block text-gray-700 text-sm font-bold mb-2" for="media">
           Nombre
         </label>
-        <input class="text-sm shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="media" type="text" placeholder="Acme News" v-model="form.name" required>
+        <input class="text-sm shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="media" type="text" placeholder="Acme News" name="name" required>
       </div>
       <div class="mb-4">
         <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
           Email de contacto
         </label>
-        <input class="text-sm shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="acme@news.com" v-model="form.email" required>
+        <input class="text-sm shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="acme@news.com" name="email" required>
       </div>
       <div class="mb-4">
         <label class="block text-gray-700 text-sm font-bold mb-2" for="description">
           Descripción
         </label>
-        <textarea class="text-sm shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="description" placeholder="Acme News es un medio de noticias explosivas" v-model="form.description" required></textarea>
+        <textarea class="text-sm shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="description" placeholder="Acme News es un medio de noticias explosivas" name="description" required></textarea>
       </div>
       <div class="mb-6">
         <label for="website" class="block text-gray-700 text-sm font-bold md">
           Website
         </label>
-        <input type="url" class="text-sm shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="https://acme.news" v-model="form.website" required>
+        <input type="url" class="text-sm shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="https://acme.news" name="website" required>
       </div>
       <div class="flex items-center justify-between">
         <button class="bg-teal-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
@@ -38,37 +38,6 @@
 
 <script>
 export default {
-  name: 'Request',
-  data () {
-    return {
-      form: {
-        name: '',
-        email: '',
-        description: '',
-        website: ''
-      }
-    }
-  },
-  methods: {
-    encode (data) {
-      return Object.keys(data)
-        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
-        .join('&')
-    },
-    handleSubmit (e) {
-      fetch('/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: this.encode({
-          'form-name': 'request',
-          ...this.form
-        })
-      })
-        .then(() => {
-          this.$router.push('feedback')
-        })
-        .catch(console.log)
-    }
-  }
+  name: 'Request'
 }
 </script>
